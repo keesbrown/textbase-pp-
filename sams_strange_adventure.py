9a8c412ee2b10dd46ab997adcad5a6c480d5b740
unknown <kees.brown1@gmail.com>
2022-10-24 14:37:18 +0200

    balls

---
+++ sams_strange_adventure.py
@@ -0,0 +1,15 @@
+
+print ("welkom to sam's strange advanture")
+print ("this is a small game made by kees brown, enjoy!")
+print ("pres enter to continue")
+input() 
+print ("""The adventure begins with sam entering a bar after he has left his home country
+he had been robbed of all his belongings by a strange boxer and now finds himself in a country unknown to him
+Sam heads to a bar and sits down.
+de bartender goes up to him and asks him wat Sam wants
+A = Ask for a gals of water 
+B = Ask for information
+C = Ask for the strongest stuff they have
+type A, B or C""")
+
+
